d6ba9b4930fbf95bf1e4d47f05eb8ca24482a471
elioar <elio.arapi02@gmail.com>
2023-09-06 09:25:52 +0300

    Test

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -26,7 +26,7 @@ const MobileSidebar = () => {
                 </Button>
             </SheetTrigger>
             <SheetContent side="left" className="p-0">
-                <Sidebar />
+
             </SheetContent>
         </Sheet>
     );
