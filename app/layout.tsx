f75e8612a7316e5dc9b3436ac9a51120466a7af6
elioar <elio.arapi02@gmail.com>
2023-09-08 05:38:33 +0300

    Dark Mode Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/components/modal-provider'
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +23,9 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider/>
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
 import { getApiLimitCount } from "@/lib/api-limit";
 import DarkModeSwitch from './DarkModeSwitch';
+import { Moon, Sun } from "lucide-react";
+import { ModeToggle } from "./Mode-toggle";
 
 const Navbar = async () => {
     const apiLimitCount = await getApiLimitCount();
@@ -9,36 +11,8 @@ const Navbar = async () => {
     return ( 
       <div className="flex items-center p-4">
         <MobileSidebar apiLimitCount={apiLimitCount} />
-        <div className="mr-6 flex w-full justify-end items-center">
-            <svg className="dark:fill-white mr-2"
-                    viewBox="0 0 129 129"
-                    width={20}
-                    height={20}
-                >
-                <g>
-                    <g>
-                    <path d="m64.5,92.6c15.5,0 28-12.6 28-28s-12.6-28-28-28-28,12.6-28,28 12.5,28 28,28zm0-47.9c11,0 19.9,8.9 19.9,19.9 0,11-8.9,19.9-19.9,19.9s-19.9-8.9-19.9-19.9c0-11 8.9-19.9 19.9-19.9z"/>
-                    <path d="m68.6,23.6v-12.9c0-2.3-1.8-4.1-4.1-4.1s-4.1,1.8-4.1,4.1v12.9c0,2.3 1.8,4.1 4.1,4.1s4.1-1.8 4.1-4.1z"/>
-                    <path d="m60.4,105.6v12.9c0,2.3 1.8,4.1 4.1,4.1s4.1-1.8 4.1-4.1v-12.9c0-2.3-1.8-4.1-4.1-4.1s-4.1,1.8-4.1,4.1z"/>
-                    <path d="m96.4,38.5l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8,0l-9.1,9.1c-1.6,1.6-1.6,4.2 0,5.8 0.8,0.8 1.8,1.2 2.9,1.2s2.1-0.4 2.9-1.2z"/>
-                    <path d="m23.5,105.6c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8,0l-9.1,9.1c-1.6,1.6-1.6,4.2 0,5.8z"/>
-                    <path d="m122.5,64.6c0-2.3-1.8-4.1-4.1-4.1h-12.9c-2.3,0-4.1,1.8-4.1,4.1 0,2.3 1.8,4.1 4.1,4.1h12.9c2.2,1.42109e-14 4.1-1.8 4.1-4.1z"/>
-                    <path d="m10.6,68.7h12.9c2.3,0 4.1-1.8 4.1-4.1 0-2.3-1.8-4.1-4.1-4.1h-12.9c-2.3,0-4.1,1.8-4.1,4.1 0,2.3 1.9,4.1 4.1,4.1z"/>
-                    <path d="m102.6,106.8c1,0 2.1-0.4 2.9-1.2 1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l9.1,9.1c0.8,0.8 1.9,1.2 2.9,1.2z"/>
-                    <path d="m38.4,38.5c1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l9.1,9.1c0.8,0.8 1.8,1.2 2.9,1.2s2.1-0.4 2.9-1.2z"/>
-                    </g>
-                </g>
-                </svg>
-            <DarkModeSwitch/>
-                <svg className="dark:fill-white ml-2"
-                        viewBox="0 0 30 25"
-                        width={20}
-                        height={20}
-                    >
-                        <path id="Moon"
-                        d="M12.79,25A12.79,12.79,0,0,1,8.93,0,.5.5,0,0,1,9.5.76,10.72,10.72,0,0,0,24.24,15.49a.5.5,0,0,1,.74.58A12.73,12.73,0,0,1,12.79,25ZM8,1.43A11.79,11.79,0,1,0,23.57,17,11.73,11.73,0,0,1,8,1.43Z"
-                        />
-                </svg>
+        <div className="mr-6 flex w-full justify-end items-center">            
+            <ModeToggle/>
         </div>
         <div>
           <UserButton afterSignOutUrl="/" />
