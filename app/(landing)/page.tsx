66360b46330d08c60332471c948f9b8b4caec26a
elioar <elio.arapi02@gmail.com>
2023-09-09 13:30:15 +0300

    Landing Page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,67 +1,15 @@
-"use client"
-
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { Switch } from "@/components/ui/switch";
-import Image from "@/node_modules/next/image";
-import { ModeToggle } from "@/components/Mode-toggle";
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
-    const [darkMode, setDarkMode] = useState(true); // Set dark mode as default
-
-    useEffect(() => {
-        if (darkMode) {
-            document.body.classList.add('dark');
-        } else {
-            document.body.classList.remove('dark');
-        }
-    }, [darkMode]);
-
-    useEffect(() => {
-        // Load user preference from localStorage on component mount
-        const storedMode = localStorage.getItem("darkMode");
-        if (storedMode !== null) {
-            setDarkMode(storedMode === "true");
-        }
-    }, []);
-
-    const toggleDarkMode = () => {
-        setDarkMode(prevMode => !prevMode);
-    };
-
-    // Update user preference in localStorage whenever darkMode changes
-    useEffect(() => {
-        localStorage.setItem("darkMode", darkMode.toString());
-    }, [darkMode]);
-
     return (
-        <div className="flex items-center mt-2">
-            <div className="ml-5 w-20 h-20 relative">
-                <Image
-                    alt="logo"
-                    fill
-                    src="/svg/logo-white.svg"
-                    className="white:bg-dark"
-                />
-            </div>
-            <div className="flex w-full justify-end mr-5">
-                <ModeToggle/>
-            </div>
-            <div>
-                <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
+        <div className="h-full">
+            <LandingNavbar />
+            <LandingHero />
+            <LandingContent />
         </div>
-    )
+    );
 }
 
 export default LandingPage;
