6b27096ae56d6d907eaf1ebc3058a2e30f4aae2d
elioar <elio.arapi02@gmail.com>
2023-09-12 12:11:15 +0300

    New colors in sing in pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,60 +1,30 @@
 "use client";
 
-import { useEffect } from 'react';
-import { Application } from '@splinetool/runtime';
+import './background.scss'; // Import the CSS file with the background styles
+import { useState } from 'react';
 
 const AuthLayout = ({
   children
 }: {
   children: React.ReactNode;
 }) => {
-  // Initialize the 3D canvas and load the Spline scene
-  useEffect(() => {
-    const canvas = document.getElementById('canvas3d') as HTMLCanvasElement;
-    if (canvas) {
-      const app = new Application(canvas);
-      app.load('https://draft.spline.design/9oPv4rdac4FNZlef/scene.splinecode');
-    }
-  }, []);
+
+  // Define a state variable to control the class name
+  const [backgroundClassName, setBackgroundClassName] = useState('background-container');
 
   return (
     <div className="auth-layout">
       <div className="centered-container">
-        <canvas className="centered-canvas" id="canvas3d"></canvas>
+        {/* Place the background here */}
+        <div className={`blur-container ${backgroundClassName}`}>
+          <div className="shape"></div>
+          <div className="shape"></div>
+        </div>
       </div>
-      <main className="content">{children}</main>
-
-      <style jsx>{`
-        .auth-layout {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-          overflow: hidden;
-          position: relative;
-        }
-
-        .centered-container {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          z-index: -1; /* Place the canvas behind the content */
-        }
-
-        .centered-canvas {
-          width: 100%;
-          height: 100%;
-        }
-
-        .content {
-          position: relative; /* Ensure the content can be positioned relative to its parent */
-          z-index: 1; /* Place the content above the canvas */
-        }
-      `}</style>
+      {/* Place the centered content here */}
+      <main className="content centered-content">{children}</main>
     </div>
   );
 };
 
 export default AuthLayout;
-
