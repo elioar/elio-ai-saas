13da120d41336ead3b137f4363bf091f2f6daef4
elioar <elio.arapi02@gmail.com>
2023-09-12 03:29:32 +0300

    fix

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -133,9 +133,18 @@ const CodePage = () => {
                 )}
               >
                 {message.role ===  "user" ? <UserAvatar /> : <BotAvatar />}
-                <SyntaxHighlighter language="javascript" style={a11yDark}>
-                  {message.content || ''}
-                </SyntaxHighlighter>
+                <ReactMarkdown components={{
+                  pre: ({ node, ...props }) => (
+                    <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                      <pre {...props} />
+                    </div>
+                  ),
+                  code: ({ node, ...props }) => (
+                    <code className="bg-black/10 rounded-lg p-1" {...props} />
+                  )
+                }} className="text-sm overflow-hidden leading-7">
+                  {message.content || ""}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
