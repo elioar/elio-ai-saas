5ea2494bc1bb022d8ad0e54401c3ed7a0d890a61
elioar <elio.arapi02@gmail.com>
2023-09-14 08:06:12 +0300

    Dashboard Page UI

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,48 +1,7 @@
 "use client";
-
-import { Card } from "@/components/ui/card";
-import { ArrowRight, Code, MessageSquare, ImageIcon, VideoIcon, Music, } from "lucide-react";
-import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
-const tools = [
-    {
-        label: "Conversation",
-        icon: MessageSquare,
-        color: "text-violet-500",
-        bgColor: "bg-violet-500/10",
-        href: "/conversation"
-    }, {
-        label: "Music Generation",
-        icon: Music,
-        color: "text-emerald-500",
-        bgColor: "bg-emerald-500/10",
-        href: "/music"
-    },
-    {
-        label: "Image Generation",
-        icon: ImageIcon,
-        color: "text-pink-700",
-        bgColor: "bg-pink-700/10",
-        href: "/image"
-    },
-    {
-        label: "Video Generation",
-        icon: VideoIcon,
-        color: "text-yellow-700",
-        bgColor: "bg-yellow-700/10",
-        href: "/video"
-    },
-    {
-        label: "Code Generation",
-        icon: Code,
-        color: "text-blue-700",
-        bgColor: "bg-blue-700/10",
-        href: "/code"
-    },
-]
+import { SendHorizontalIcon } from "lucide-react";
 
 const DashboardPage = () => {
     const router = useRouter();
@@ -64,7 +23,7 @@ const DashboardPage = () => {
                             style={{
                                 backgroundImage: `url('https://images.unsplash.com/photo-1626428091984-48f9ffbf927c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1866&q=80')`,
                                 backgroundSize: 'cover',
-                                height: '250px',
+                                height: '200px',
                             }}
                         >
                             <div
@@ -81,7 +40,7 @@ const DashboardPage = () => {
                             style={{
                                 backgroundImage: `url('https://images.unsplash.com/photo-1629946832022-c327f74956e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80')`,
                                 backgroundSize: 'cover',
-                                height: '250px',
+                                height: '200px',
                             }}
                         >
                             <div
@@ -98,7 +57,7 @@ const DashboardPage = () => {
                             style={{
                                 backgroundImage: `url('https://images.unsplash.com/photo-1633907284646-7abf4a195875?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80')`,
                                 backgroundSize: 'cover',
-                                height: '250px',
+                                height: '200px',
                             }}
                         >
                             <div
@@ -115,7 +74,7 @@ const DashboardPage = () => {
                             style={{
                                 backgroundImage: `url('https://images.unsplash.com/photo-1650018943477-781416d478cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80')`,
                                 backgroundSize: 'cover',
-                                height: '250px',
+                                height: '200px',
                             }}
                         >
                             <div
@@ -130,17 +89,20 @@ const DashboardPage = () => {
                 </div>
             <div>
                 <Link href="/code">
-                    <div className="relative group transition-transform transform scale-100 group-hover:scale-105 rounded-xl hover:scale-105"
+                    <div className="bg-black relative group transition-transform transform scale-100 group-hover:scale-105 rounded-xl hover:scale-105"
                             style={{
-                            backgroundImage: `url('https://images.unsplash.com/photo-1559838536-082a0c488adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80')`,
+                            backgroundImage: `url('https://images.unsplash.com/photo-1618029529501-9adf93df3e84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')`,
                             backgroundSize: 'cover',
                             height: '100px',
                         }}
                     >                      
                     <div className="absolute inset-0 backdrop-blur-md backdrop-filter duration-300 opacity-0 group-hover:opacity-100 rounded-xl"></div>
-                        <p className="absolute inset-0 flex items-center justify-center text-white font-bold text-2xl">
+                        <p className="ml-12 absolute inset-0 flex items-center justify-left text-white font-bold text-2xl">
                             Code Generation
                         </p>
+                        <div className="mr-12 absolute inset-0 flex items-center justify-end text-white font-bold text-2xl">
+                            <SendHorizontalIcon />
+                        </div>
                     </div>
                 </Link>
             </div>
