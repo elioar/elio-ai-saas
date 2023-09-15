63c7d00a27ef6ddc5d784c749d66ef91a9712873
elioar <elio.arapi02@gmail.com>
2023-09-15 22:35:44 +0300

    Change: The Logo

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -20,16 +20,22 @@ export const LandingNavbar = () => {
     return (
         <nav className="p-4 bg-transparent flex items-center justify-between">
             <Link href="/" className="flex items-center">
-                <div className="relative h-8 w-8 mr-4">
+                <div className="relative h-8 w-8 mr-2">
                     <Image
-                        fill
+                        height="30"
+                        width="25"
                         alt="Logo"
-                        src="/logo.png"
+                        src="/lighting.png"
                     />
                 </div>
-                <h1 className={cn("text-2xl font-bold", font.className)}>
-                    Elio Ai
-                </h1>
+                <div className="flex-1 mt-2">
+                    <Image
+                        width="80"
+                        height="50"
+                        alt="logo"
+                        src="./svg/eixai-w.svg"
+                    />
+                </div>
             </Link>
             <div className="flex items-center gap-x-2">
                 <ModeToggle />
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -70,14 +70,15 @@ const Sidebar = ({
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white ">
         <div className="px-3 py-2 flex-1">
             <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-                <div className="relative w-8 h-8 mr-4">
+                <div className="relative w-8 h-8 mr-6">
                     <Image 
-                        fill
+                        width="30"
+                        height="30"
                         alt="Logo"
-                        src="/logo.png"
+                        src="/lighting.png"
                     />
                 </div>
-                <div className=" py-2 flex-1">
+                <div className="flex-1 mt-4">
                     <Image
                         width="80"
                         height="50"
